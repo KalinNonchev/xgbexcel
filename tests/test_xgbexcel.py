6592c52d77d98cb276032f583deb413027d9904c
Kalin Nonchev <50597791+KalinNonchev@users.noreply.github.com>
2025-08-20 19:13:24 +0300

    add test

--- tests/test_xgbexcel.py
+++ tests/test_xgbexcel.py
@@ -6,13 +6,13 @@ from xgbexcel import XGBtoExcel
 # Generate the data to fit the model
 X = [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10],
               [11, 1], [13, 3], [1, 16], [17, 3], [19, 20]]
-y = [100, 1, 2, 2, 1, 1, 2, 2, 1, 2]
+y = [0, 1, 2, 2, 1, 1, 2, 2, 1, 2]
 
-# Fit the XGBoost model
-model = xgb.XGBRegressor(n_estimators=2, max_depth=1)
-model.fit(X, y)
 
-def test_xgb_to_excel_expr():
+def test_xgb_regressor_to_excel_expr():
+    # Fit the XGBoost Regressor model
+    model = xgb.XGBRegressor(n_estimators=2, max_depth=1)
+    model.fit(X, y)
     # Initialize the XGBtoExcel object
     xgb_to_excel = XGBtoExcel(model)
     print(xgb_to_excel.expression)
@@ -20,9 +20,25 @@ def test_xgb_to_excel_expr():
     assert isinstance(xgb_to_excel.expression, str)
     
     # Test that the expression attribute is not empty
-    assert xgb_to_excel.expression == "((IF((x1<=2),10.1000004,0.266666681)+IF((x2<=2.5),8.96333408,-0.0988889039))+0.5)"
+    assert xgb_to_excel.expression == "((IF((x2<=2.5),0,0.300000012)+IF((x2<=2.5),0,0.220000014))+0.5)"
+
+def test_xgb_clf_to_excel_expr():
+    # Fit the XGBoost Classifier model
+    model = xgb.XGBClassifier(n_estimators=2, max_depth=1)
+    model.fit(X, y)
+    # Initialize the XGBtoExcel object
+    xgb_to_excel = XGBtoExcel(model)
+    print(xgb_to_excel.expression)
+    # Test that the expression attribute is a string
+    assert isinstance(xgb_to_excel.expression, str)
+    
+    # Test that the expression attribute is not empty
+    assert xgb_to_excel.expression == "(EXP(((IF((x1<=4),-7.66345476e-09,-0.170270279)+IF((x1<=4),-0.00112346245,-0.153451324))+0.5))/(EXP(((IF((x1<=4),-7.66345476e-09,-0.170270279)+IF((x1<=4),-0.00112346245,-0.153451324))+0.5))+EXP(((IF((x1<=8),-0.0620689802,0.124137931)+IF((x2<=5),0.107327893,-0.0584098995))+0.5))+EXP(((IF((x2<=5),-0.0620689802,0.217241377)+IF((x2<=5),-0.0582885109,0.178286016))+0.5)))) , (EXP(((IF((x1<=8),-0.0620689802,0.124137931)+IF((x2<=5),0.107327893,-0.0584098995))+0.5))/(EXP(((IF((x1<=4),-7.66345476e-09,-0.170270279)+IF((x1<=4),-0.00112346245,-0.153451324))+0.5))+EXP(((IF((x1<=8),-0.0620689802,0.124137931)+IF((x2<=5),0.107327893,-0.0584098995))+0.5))+EXP(((IF((x2<=5),-0.0620689802,0.217241377)+IF((x2<=5),-0.0582885109,0.178286016))+0.5)))) , (EXP(((IF((x2<=5),-0.0620689802,0.217241377)+IF((x2<=5),-0.0582885109,0.178286016))+0.5))/(EXP(((IF((x1<=4),-7.66345476e-09,-0.170270279)+IF((x1<=4),-0.00112346245,-0.153451324))+0.5))+EXP(((IF((x1<=8),-0.0620689802,0.124137931)+IF((x2<=5),0.107327893,-0.0584098995))+0.5))+EXP(((IF((x2<=5),-0.0620689802,0.217241377)+IF((x2<=5),-0.0582885109,0.178286016))+0.5))))"
+
 
 def test_rename_features():
+    model = xgb.XGBRegressor(n_estimators=2, max_depth=1)
+    model.fit(X, y)
     # Initialize the XGBtoExcel object
     xgb_to_excel = XGBtoExcel(model)
     # Test that the rename_features method works
@@ -36,6 +52,8 @@ def test_rename_features():
         assert new_name in xgb_to_excel.expression, new_name
 
 def test_save_expr():
+    model = xgb.XGBRegressor(n_estimators=2, max_depth=1)
+    model.fit(X, y)
     # Initialize the XGBtoExcel object
     xgb_to_excel = XGBtoExcel(model)
     # Test that the save_expr method works
